b7fb041610d00728797f0ee650cb113fb4244a88
givsty <golovkovvigg@gmail.com>
2023-07-07 19:54:35 +0300

    fix Total

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btn = document.querySelector('#btn')
 const result = document.querySelector('#result')
 const total = document.querySelector("#total")
 const deleteEvent = document.querySelector(".delete")
+let i = 0
 
 btn.addEventListener('click', ()=>{
   if(!input.value) return ''
@@ -11,10 +12,9 @@ btn.addEventListener('click', ()=>{
 })
 
 function createDeleteElements() {
-
+  i++;
   const li = document.createElement('li')
   const btn = document.createElement('button')
-
   li.className = 'li'
   li.textContent = input.value
 
@@ -23,8 +23,10 @@ function createDeleteElements() {
   li.appendChild(btn)
 
   btn.addEventListener('click', ()=>{
+    i--;
+    total.textContent = i;
     result.removeChild(li)
   })
-
+  total.textContent = i;
   result.appendChild(li)
 }
